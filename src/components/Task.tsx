6c6ee7a47305c943c10fbb2132243eb3fd1f5f62
Samaresh Das <samareshmail679@gmail.com>
2023-07-09 21:25:19 +0530

    Added add and remove todos functionality, Added MST model function to edit todo, Cleaned the UI with tailwind

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,23 +1,36 @@
 "use client";
 import { store } from "@/store/TaskStore";
+import { observer } from "mobx-react-lite";
 import React from "react";
+import TaskList from "./TaskList";
 
-const Task = () => {
+const Task = observer(() => {
   const handleToggle = (todo: any) => {
     todo.toggle("Done");
   };
+
+  const todoRemoveHandler = (id: string) => {
+    store.removeTodoById(id);
+  };
+
+  const todoEditHandler = (id: string, title: string, desc: string) => {
+    store.removeTodoById(id);
+  };
+
   return (
-    <div>
+    <ul className="list-none my-[2rem] mx-auto p-0 w-[40rem]">
       {store.todos.map((todo, i) => (
-        <div key={i}>
-          <p>{todo.title}</p>
-          <p>{todo.desc}</p>
-          <p>{todo.status}</p>
-          <button onClick={() => handleToggle(todo)}>Done</button>
-        </div>
+        <TaskList
+          key={i}
+          changeStatus={() => handleToggle(todo)}
+          title={todo.title}
+          desc={todo.desc}
+          status={todo.status}
+          removeTodo={() => todoRemoveHandler(todo.id)}
+        />
       ))}
-    </div>
+    </ul>
   );
-};
+});
 
 export default Task;
