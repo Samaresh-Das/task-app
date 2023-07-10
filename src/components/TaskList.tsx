472ae19fa6c5e5b0bc7aba997d14570629ec1396
Samaresh Das <samareshmail679@gmail.com>
2023-07-10 15:22:18 +0530

    edit to-do functionality added

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -8,7 +8,8 @@ const TaskList: React.FC<{
   status: string;
   changeStatus: () => void;
   removeTodo: () => void;
-}> = ({ title, desc, status, changeStatus, removeTodo }) => {
+  showModal: () => void;
+}> = ({ title, desc, status, changeStatus, removeTodo, showModal }) => {
   return (
     <li className="my-[1rem] mx-0  shadow-[0_1px_4px_rgba(0,0,0,0.2)] p-[1rem] bg-[#f7f5ef] relative">
       <h2>{title}</h2>
@@ -19,6 +20,12 @@ const TaskList: React.FC<{
         className="font-inherit bg-[#ebb002] border-[#ebb002] text-[#201d0f] px-2 py-1 rounded-md cursor-pointer"
       >
         Done
+      </button>
+      <button
+        onClick={showModal}
+        className="font-inherit bg-[#ebb002] border-[#ebb002] text-[#201d0f] px-2 py-1 rounded-md cursor-pointer"
+      >
+        Edit
       </button>
       <button
         className="absolute right-2 inset-y-1/4 text-[30px]"
